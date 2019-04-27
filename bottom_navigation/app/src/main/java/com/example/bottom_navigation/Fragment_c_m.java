a8b6ebfefdd684787880d91da11fac511a36e3dc
StrangeNoob <sipun63427452@gmail.com>
2019-04-27 12:01:16 +0530

    style Update

---
+++ bottom_navigation/app/src/main/java/com/example/bottom_navigation/Fragment_c_m.java
@@ -0,0 +1,16 @@
+package com.example.bottom_navigation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_c_m extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragement_c_m, container, false);
+    }
+}
---
+++ bottom_navigation/app/src/main/java/com/example/bottom_navigation/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.bottom_navigation;
+
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View configurationActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView botNav = findViewById(R.id.bottom_navigation);
+        botNav.setOnNavigationItemSelectedListener(navlistener);
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    Fragment selectedFragment= null;
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.home:
+                            selectedFragment = new Fragment_home();
+                            break;
+                        case R.id.c_m:
+                            selectedFragment = new Fragment_c_m();
+                            break;
+                        case R.id.farmer:
+                            selectedFragment = new Fragment_farmerlist();
+                            break;
+                        case R.id.profile:
+                            selectedFragment = new Fragment_profile();
+                            break;
+                        case R.id.service:
+                            selectedFragment = new Fragment_service();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+
+            };
+}
