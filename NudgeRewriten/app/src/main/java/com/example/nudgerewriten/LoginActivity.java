5e7facb48509e350fcf535a1dbc431c58e4ed2d3
StrangeNoob <sipun63427452@gmail.com>
2019-05-02 16:51:21 +0530

    loginpage Update

--- NudgeRewriten/app/src/main/java/com/example/nudgerewriten/LoginActivity.java
+++ NudgeRewriten/app/src/main/java/com/example/nudgerewriten/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,40 +15,30 @@ public class LoginActivity extends AppCompatActivity {
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String TEXT = "text";
     private EditText licencecode;
-    private Button savebtn;
+    private final TextWatcher mTextEditorWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
 
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //This sets a textview to the current length
+
+        }
+
+        public void afterTextChanged(Editable s) {
+            if (s.length() == 8) {
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        }
+    };
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final SharedPreferences msharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-
-        if (msharedPreferences.contains(SHARED_PREFS))
-        {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+        setContentView(R.layout.activity_login);
+        licencecode = (EditText) findViewById(R.id.licenceCode);
+        licencecode.addTextChangedListener(mTextEditorWatcher);
         }
-        else
-        {
-            setContentView(R.layout.activity_login);
-            licencecode = (EditText) findViewById(R.id.licenceCode);
-            savebtn = (Button) findViewById(R.id.submitButton);
-            savebtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    savedata();
-                }
-                public void savedata() //To save Data in SharePreferences
-                {
-                    SharedPreferences.Editor editor = msharedPreferences.edit();
-                    editor.putString(TEXT, licencecode.getText().toString());
-                    editor.apply();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-
-        }
-    }
 }
