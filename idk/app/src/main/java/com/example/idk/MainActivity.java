5e2b2fa6eb64f3fb0b0ef4133dc88f74ab9026c2
StrangeNoob <sipun63427452@gmail.com>
2019-06-01 16:22:59 +0530

    Profile Page Remaster

--- idk/app/src/main/java/com/example/idk/MainActivity.java
+++ idk/app/src/main/java/com/example/idk/MainActivity.java
@@ -10,9 +10,6 @@ import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private static Toolbar toolbar;
-    private static CollapsingToolbarLayout collapsingToolbarLayout;
-    int mutedColor = R.attr.colorPrimary;
 
 
     @Override
@@ -20,27 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
-        collapsingToolbarLayout.setTitle("Ram Lal");//Set Title over collapsing toolbar layout
-
-
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
-                R.drawable.farmerpic2);
-
-        // It will generate colors based on the image in an AsyncTask.
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @SuppressWarnings("ResourceType")
-            @Override
-            public void onGenerated(Palette palette) {
-
-                mutedColor = palette.getMutedColor(R.color.colorPrimary);
-                collapsingToolbarLayout.setContentScrimColor(mutedColor);
-                collapsingToolbarLayout.setStatusBarScrimColor(R.color.design_default_color_primary);
-            }
-        });
     }
 }
