030793ab117f3f38659cf43934b2b6bc7a0c2a4e
StrangeNoob <sipun63427452@gmail.com>
2019-06-07 12:22:42 +0530

    OnLongClick working

--- NudgeRewriten/app/src/main/java/com/example/nudgerewriten/activities/ViewStore.java
+++ NudgeRewriten/app/src/main/java/com/example/nudgerewriten/activities/ViewStore.java
@@ -6,6 +6,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.example.nudgerewriten.R;
 import com.example.nudgerewriten.adapters.ProductAdapter;
@@ -15,34 +19,106 @@ import com.example.nudgerewriten.models.products;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewStore extends AppCompatActivity {
+public class ViewStore extends AppCompatActivity implements View.OnLongClickListener {
 
+    public boolean is_item_checked=false;
+    TextView counterTextview;
     public RecyclerView prodRcvId;
     public RecyclerView.LayoutManager layoutManager;
     List<products> Products = new ArrayList<>();
     private Toolbar toolbar;
     private ProductViewAdapter adapter;
-
+    ArrayList<products> selectionlist;
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_store);
-        for(int i=0;i<6; i++ ) {
-            Products.add(new products("Product "+"i","Company "+ "i",(1+i)*100 ,R.drawable.fertilizer));
-        }
-        toolbar= findViewById(R.id.itempurchage);
+        toolbar= (Toolbar) findViewById(R.id.itempurcharge);
         setSupportActionBar(toolbar);
+        counterTextview=(TextView) findViewById(R.id.counterItem);
+        counterTextview.setVisibility(View.GONE);
+        for(int i=0;i<6; i++ ) {
+            Products.add(new products("Product "+i,"Company "+i,(1+i)*100 ,R.drawable.fertilizer));
+        }
+        selectionlist = new ArrayList<>();
         prodRcvId=findViewById(R.id.recyclerViewStore);
         layoutManager = new LinearLayoutManager(this);
         prodRcvId.setHasFixedSize(true);
         prodRcvId.setLayoutManager(layoutManager);
-        adapter = new ProductViewAdapter(Products,ViewStore.this);
+        adapter = new ProductViewAdapter(Products,this);
         prodRcvId.setAdapter(adapter);
     }
 
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.itemselect_menu,menu);
-        return super.onCreateOptionsMenu(menu);
+    public boolean onLongClick(View v) {
+        toolbar.getMenu().clear();
+        toolbar.inflateMenu(R.menu.itemselect_menu);
+        counterTextview.setVisibility(View.VISIBLE);
+        is_item_checked=true;
+        adapter.notifyDataSetChanged();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        return true;
+    }
+    public void prepareselection(View view,int pos){
+
+        if(((CheckBox)view).isChecked()){
+            selectionlist.add(Products.get(pos));
+            count +=1;
+            updateCounnter(count);
+        }
+        else {
+            selectionlist.remove(Products.get(pos));
+            count-=1;
+            updateCounnter(count);
+        }
+
+    }
+    public void updateCounnter(int count)
+    {
+        if(count == 0)
+        {
+            counterTextview.setText("0 item Selected");
+        }
+        else {
+            counterTextview.setText(count+"item Selected");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.item_delete)
+        {
+            clearAction();
+            ProductViewAdapter productadapter= (ProductViewAdapter) adapter;
+            productadapter.updateAdapter(selectionlist);
+
+        }
+        else if (item.getItemId()== android.R.id.home)
+        {
+            clearAction();
+            adapter.notifyDataSetChanged();
+        }
+        return true;
+    }
+
+    public void clearAction(){
+        is_item_checked= false;
+        toolbar.getMenu().clear();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        counterTextview.setText(View.GONE);
+        counterTextview.setText("0 item Selected");
+    }
+    @Override
+    public void onBackPressed() {
+        if (is_item_checked) {
+            clearAction();
+            adapter.notifyDataSetChanged();
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
--- NudgeRewriten/app/src/main/java/com/example/nudgerewriten/adapters/ProductViewAdapter.java
+++ NudgeRewriten/app/src/main/java/com/example/nudgerewriten/adapters/ProductViewAdapter.java
@@ -2,6 +2,7 @@ package com.example.nudgerewriten.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,15 +12,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.nudgerewriten.R;
-import com.example.nudgerewriten.activities.MainActivity;
 import com.example.nudgerewriten.activities.ViewStore;
 import com.example.nudgerewriten.models.products;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductViewAdapter extends RecyclerView.Adapter<ProductViewAdapter.ProductViewHolder> {
 
-    private List<products> Products;
+    public List<products> Products;
     ViewStore viewStore;
     Context context;
 
@@ -44,6 +45,14 @@ public class ProductViewAdapter extends RecyclerView.Adapter<ProductViewAdapter.
         viewHolder.Price.setText(Integer.toString(Products.get(i).getPrice()));
         viewHolder.Productname.setText(Products.get(i).getProductname());
         viewHolder.Productcompany.setText(Products.get(i).getProductComany());
+        if(!viewStore.is_item_checked)
+        {
+            viewHolder.checkBox.setVisibility(View.GONE);
+        }
+        else {
+            viewHolder.checkBox.setVisibility(View.VISIBLE);
+            viewHolder.checkBox.setChecked(false);
+        }
     }
 
     @Override
@@ -51,19 +60,39 @@ public class ProductViewAdapter extends RecyclerView.Adapter<ProductViewAdapter.
         return Products.size();
     }
 
-    public static class ProductViewHolder extends RecyclerView.ViewHolder
+    public static class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
     {
+
         ImageView itemimage;
         TextView Productname,Productcompany,Price;
         CheckBox checkBox;
         ViewStore viewStore;
+        CardView cardView;
         public ProductViewHolder(@NonNull View itemView, ViewStore viewStore) {
             super(itemView);
             itemimage=(ImageView) itemView.findViewById(R.id.viewstoreimage);
             Productname=(TextView) itemView.findViewById(R.id.viewproduct);
             Productcompany =(TextView) itemView.findViewById(R.id.viewproductcompany);
             Price=(TextView) itemView.findViewById(R.id.priceset);
+            checkBox=(CheckBox) itemView.findViewById(R.id.checkeditems);
+            cardView = (CardView) itemView.findViewById(R.id.eachproduct);
+            cardView.setOnLongClickListener(viewStore);
             this.viewStore = viewStore;
+            checkBox.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            viewStore.prepareselection(v,getAdapterPosition());
+        }
+
+    }
+    public void updateAdapter(ArrayList<products> list)
+    {
+        for (products products: list)
+        {
+            Products.remove(products);
+        }
+        notifyDataSetChanged();
     }
 }
